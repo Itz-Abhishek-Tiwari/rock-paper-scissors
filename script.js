29c9a0fea3ec48118d7d47efce0e344dc0c1426f
Abhishek Tiwari <abhitiwariabhi5@gmail.com>
2024-04-07 18:20:03 +0530

    added all player winding case

--- script.js
+++ script.js
@@ -1,12 +1,37 @@
 function getComputerChoice() {
   let choice = ["Rock", "Paper", "Scissors"];
   let randomChoice = Math.floor(Math.random() * 3);
-  let ComputerChoice;
+  let computerSelection;
   for (let i = 0; i <= choice.length; i++) {
-    if (i == randomChoice) {
-      return (ComputerChoice = choice[i]);
+    if (i === randomChoice) {
+      return (computerSelection = choice[i]);
     }
   }
+  return computerSelection;
 }
-console.log(getComputerChoice());
-console.log(" ");
+function userChoice() {
+  return prompt("Enter your choice", "").toLowerCase();
+}
+
+let computerSelection = getComputerChoice();
+console.log(computerSelection);
+let playerSelection = userChoice();
+
+function playRound(computerSelection, playerSelection) {
+  if (computerSelection == playerSelection) {
+    alert(`You and Bot choice are same so it's a tie ${computerSelection}`);
+  }
+
+  // Player wining case
+  else if (playerSelection === "rock" || computerSelection === "scissors") {
+    alert(`You Win! ${playerSelection} beats ${computerSelection} `);
+  } else if (playerSelection === "paper" || computerSelection === "rock") {
+    alert(`You Win! ${playerSelection} beats ${computerSelection} `);
+  } else if (playerSelection === "scissors" || computerSelection === "paper") {
+    alert(`You Win! ${playerSelection} beats ${computerSelection} `);
+  }
+
+  // Player losing case
+}
+
+playRound(computerSelection, playerSelection);
