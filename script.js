aa2614ae1af707fbb8845364a5f1b9c098408cff
Abhishek Tiwari <abhitiwariabhi5@gmail.com>
2024-04-08 13:00:40 +0530

    fix the issue in count

--- script.js
+++ script.js
@@ -1,38 +1,53 @@
 function getComputerChoice() {
+  let randomChoice = parseInt(Math.random() * 3);
   let choice = ["rock", "paper", "scissors"];
-  let randomChoice = Math.floor(Math.random() * 3);
   let computerSelection;
-  for (let i = 0; i <= choice.length; i++) {
-    if (i === randomChoice) {
-      return (computerSelection = choice[i]);
+  for (let i = 0; i < choice.length; i++) {
+    if (randomChoice === i) {
+      computerSelection = choice[i];
     }
   }
   return computerSelection;
 }
-function userChoice() {
-  return prompt(
-    `Enter Your choice b/w "rock", "paper", "scissors`,
-    ""
-  ).toLowerCase();
-}
 
-let computerSelection = getComputerChoice();
-console.log(computerSelection);
-let playerSelection = userChoice();
 let result;
-
+let count = 0;
 function playRound(computerSelection, playerSelection) {
-  if (computerSelection == playerSelection) {
-    return (result = `You and Bot choice are same so it's a tie ${computerSelection}, ${playerSelection}`);
+  if (computerSelection === playerSelection) {
+    result;
   } else if (
     (playerSelection === "rock" && computerSelection === "scissors") ||
     (playerSelection === "paper" && computerSelection === "rock") ||
     (playerSelection === "scissors" && computerSelection === "paper")
   ) {
-    return (result = `You win! ${playerSelection} beats ${computerSelection}`);
+    count++;
+    result;
   } else {
-    return (result = `You lose! ${computerSelection} beats ${playerSelection}`);
+    if (count <= 0) {
+      count;
+    } else {
+      count--;
+    }
+    result;
+  }
+  return result;
+}
+
+function playGame() {
+  for (let i = 0; i < 5; i++) {
+    let computerSelection = getComputerChoice();
+    console.log(computerSelection);
+    let playerSelection = prompt("enter chpice");
+    let playerData = playRound(computerSelection, playerSelection);
   }
 }
 
-console.log(playRound(computerSelection, playerSelection));
+playGame();
+function finalCount() {
+  if (count >= 3) {
+    alert(`Your Final count is: ${count} You Won!`);
+  } else {
+    alert(`Your Final count is: ${count} You Lose!`);
+  }
+}
+finalCount();
