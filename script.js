ac1056f8c6f5f9a40035a9666ca5bc14c8200d41
Abhishek Tiwari <abhitiwariabhi5@gmail.com>
2024-04-13 22:16:09 +0530

    now gui is fixed and the whole game

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let count = 0;
+let roundPlayed = 0;
+
+// Get Computer Choice
 function getComputerChoice() {
   let randomChoice = parseInt(Math.random() * 3);
   let choice = ["rock", "paper", "scissors"];
@@ -10,44 +14,90 @@ function getComputerChoice() {
   return computerSelection;
 }
 
-let result;
-let count = 0;
+// Check Computer Choice and Player Choice
 function playRound(computerSelection, playerSelection) {
   if (computerSelection === playerSelection) {
-    result;
   } else if (
     (playerSelection === "rock" && computerSelection === "scissors") ||
     (playerSelection === "paper" && computerSelection === "rock") ||
     (playerSelection === "scissors" && computerSelection === "paper")
   ) {
     count++;
-    result;
   } else {
     if (count <= 0) {
       count;
     } else {
       count--;
     }
-    result;
   }
-  return result;
+
+  roundPlayed++;
+  if (roundPlayed >= 5) {
+    if (count >= 3) {
+      document.querySelector(".result").textContent = ` You Win`;
+    } else {
+      document.querySelector(".result").textContent = ` You Loss`;
+    }
+
+    finalCount();
+  }
 }
 
+// Choice Rock
+document.querySelector(".rockBtn").addEventListener("click", () => {
+  let computerSelection = getComputerChoice();
+  playRound(computerSelection, "rock");
+});
+
+// Choice Paper
+document.querySelector(".paperBtn").addEventListener("click", () => {
+  let computerSelection = getComputerChoice();
+  playRound(computerSelection, "paper");
+});
+
+// Choice Scissors
+document.querySelector(".scissorsBtn").addEventListener("click", () => {
+  let computerSelection = getComputerChoice();
+  playRound(computerSelection, "scissors");
+});
+
+// Play Game Five Time
+/*
 function playGame() {
   for (let i = 0; i < 5; i++) {
     let computerSelection = getComputerChoice();
     console.log(computerSelection);
     let playerSelection = prompt("enter chpice");
-    let playerData = playRound(computerSelection, playerSelection);
+    playRound(computerSelection, playerSelection);
   }
 }
-
 playGame();
+*/
+
+// Check If you have  more or equal to 3 count so it can delater you win or lose.
 function finalCount() {
-  if (count >= 3) {
-    alert(`Your Final count is: ${count} You Won!`);
-  } else {
-    alert(`Your Final count is: ${count} You Lose!`);
+  //   if (count >= 3) {
+  //     console.log(`Your Final count is: ${count} You Won!`);
+  //   } else if (count <= 2) {
+  //     console.log(`Your Final count is: ${count} You Lose!`);
+  //   }
+
+  switch (count) {
+    case 0:
+    case 1:
+    case 2: {
+      document.querySelector(".currentPoint").textContent = ` ${count} `;
+      break;
+    }
+
+    case 3:
+    case 4:
+    case 5: {
+      document.querySelector(".currentPoint").textContent = ` ${count} `;
+      break;
+    }
   }
+
+  count = 0;
+  roundPlayed = 0;
 }
-finalCount();
